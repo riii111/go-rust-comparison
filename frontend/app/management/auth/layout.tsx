c257688334c1556044b7504ab631863b3d17bbfd
ryo-ichinose <ryo_ichinose@pj100.biz>
2024-11-21 17:21:07 +0900

    微修正

--- frontend/app/management/auth/layout.tsx
+++ frontend/app/management/auth/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({
     children: React.ReactNode
 }) {
     return (
-        <div className="min-h-screen w-full flex items-center justify-center bg-[#FAFAFA]">
+        <div className="min-h-screen w-full flex items-center justify-center bg-gray-50">
             <BrandingSection />
             {children}
         </div>
