558e9b630e2e21b3f595c359ee6b4069df7cd5c8
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-11-29 17:10:55 +0900

    名前変更

--- frontend/components/layout/auth/BrandingSection/contents/LineChart.tsx
+++ frontend/components/layout/auth/BrandingSection/contents/LineChart.tsx
@@ -3,14 +3,14 @@
 import { LineChart, Line } from 'recharts';
 
 const LineChartComponent = () => {
-    const data = [
+    const detarameData = [
         { value: 30 }, { value: 40 }, { value: 35 }, { value: 50 },
         { value: 45 }, { value: 55 }, { value: 48 }, { value: 52 }
     ];
 
     return (
         <div className="h-16">
-            <LineChart width={300} height={60} data={data}>
+            <LineChart width={300} height={60} data={detarameData}>
                 <Line
                     type="monotone"
                     dataKey="value"
