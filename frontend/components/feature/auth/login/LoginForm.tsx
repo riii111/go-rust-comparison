09c7156840b2d83690c5a8aef1d539d92696aa1b
Ryo Ichinose <131587765+riii111@users.noreply.github.com>
2024-11-29 16:47:06 +0900

    ルーティング不要（サーバ側で実行

--- frontend/components/feature/auth/login/LoginForm.tsx
+++ frontend/components/feature/auth/login/LoginForm.tsx
@@ -51,7 +51,6 @@ export default function LoginForm() {
                             formErrors: [result.error || MESSAGES.login.failed]
                         });
                     }
-                    router.push('/management/dashboard');
                 })();
             });
         }
