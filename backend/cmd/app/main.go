4c978b690b23c3795edceb803f09814f4c4c27bb
sotaheavymetal21 <euro246seeker@gmail.com>
2024-12-02 22:15:47 +0900

    main.goにDB初期化処理を追加

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"net/http"
 
+	"auth-system/internal/adapter/database"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// データベースを初期化
+	database.InitDB()
+
+	// ginのインスタンスを作成
 	r := gin.Default()
 	r.GET("/api/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
