6e5d3fd268acc533726d89c605cb0f43d2079033
ryo-ichinose <ryo_ichinose@pj100.biz>
2024-12-10 15:04:56 +0900

    Update main.go

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -9,7 +9,6 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.SetTrustedProxies([]string{"127.0.0.1"})	// TODO: あとでなおす
 	r.GET("/api/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"msg": "pass",
