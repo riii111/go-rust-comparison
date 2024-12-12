2004d6d6918fa20f6faf4679824390d4c92e4e85
yuu0305 <segyayuu8@gmail.com>
2024-12-12 20:46:01 +0900

    corsの基本設定

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,20 +1,38 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "log"
+    "net/http"
 
-	"github.com/gin-gonic/gin"
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/api/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"msg": "pass",
-		})
-	})
-	if err := r.Run(":8000"); err != nil {
-		log.Fatalf("サーバの起動に失敗しました: %v", err)
-	}
+    r := gin.Default()
+
+	// corsの設定
+    // TODO: 別の場所で設定する？
+	r.Use(cors.New(cors.Config{
+		// TODO: 本番のurlも追加しようね
+        AllowOrigins:     []string{"http://localhost:3000"},
+        AllowMethods:     []string{"GET", "PATCH", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length", "X-CSRF-Token"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    }))
+	
+    
+    // ヘルスチェックエンドポイント
+    r.GET("/api/health", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "msg": "pass",
+        })
+    })
+
+
+    if err := r.Run(":8000"); err != nil {
+        log.Fatalf("サーバの起動に失敗しました: %v", err)
+    }
 }
