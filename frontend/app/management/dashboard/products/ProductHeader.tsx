6d2d6dca627dfcac3b57fbc47c6551f7ae25bedc
ryo-ichinose <ryo_ichinose@pj100.biz>
2024-12-16 11:34:20 +0900

    商品ページヘッダのレンダリング改善
    新規登録ボタンをクリックするなどした際に、検索バーが過剰にレンダリングされていたので以下対応
    - 新規登録ボタンと検索バーをコンポーネント分離
    - ProductHeaderActionは削除

--- frontend/app/management/dashboard/products/ProductHeader.tsx
+++ frontend/app/management/dashboard/products/ProductHeader.tsx
@@ -1,10 +1,12 @@
-import { ProductHeaderActions } from "@/app/management/dashboard/products/ProductHeaderActions"
+import { ProductSearch } from "@/components/feature/dashboard/products/search/ProductSearch"
+import { ProductCreateButton } from "@/components/feature/dashboard/products/button/ProductCreateButton"
 
 export function ProductHeader() {
     return (
         <div className="flex flex-col gap-4">
-            <div className="flex items-center justify-end">
-                <ProductHeaderActions />
+            <div className="flex items-center justify-between w-full gap-4">
+                <ProductSearch />
+                <ProductCreateButton />
             </div>
         </div>
     )
