a1cf9ec81603f19ea514bdee4c549b1d3eeccd60
yuu0305 <segyayuu8@gmail.com>
2024-12-22 03:33:05 +0900

    インデントどないしたん

--- backend/internal/domain/models/products.go
+++ backend/internal/domain/models/products.go
@@ -4,23 +4,23 @@ import (
     "time"
 
     "github.com/google/uuid"
-	"github.com/shopspring/decimal"
+    "github.com/shopspring/decimal"
     "gorm.io/gorm"
 )
 
 type Product struct {
-    ID           string    			`gorm:"type:uuid;primaryKey" json:"id"`
-    Name         string    			`gorm:"not null" json:"name" validate:"required`
-    Description  string    			`gorm:"type:text;not null" json:"description" validate:"required`
-    MaterialInfo string    			`gorm:"type:text;not null" json:"material_info" validate:"required`
-    BasePrice    decimal.Decimal	`gorm:"type:decimal(10,2);not null" json:"base_price" validate:"required"`
-    Category     string    			`gorm:"not null" json:"category" validate:"required"`
-    ImageURLs    []string  			`gorm:"type:text[]" json:"image_urls"`
-	CreatedBy     string    		`gorm:"type:uuid" json:"created_by" validate:"required,uuid"`
-	UpdatedBy     string    		`gorm:"type:uuid" json:"updated_by" validate:"required,uuid"`
-    CreatedAt    time.Time 			`gorm:"autoCreateTime" json:"created_at"`
-    UpdatedAt    time.Time 			`gorm:"autoUpdateTime" json:"updated_at"`
-    Stocks       []Stock   			`gorm:"constraint:OnDelete:SET NULL" json:"stocks"`
+    ID           string          `gorm:"type:uuid;primaryKey" json:"id"`
+    Name         string          `gorm:"not null" json:"name" validate:"required"`
+    Description  string          `gorm:"type:text;not null" json:"description" validate:"required"`
+    MaterialInfo string          `gorm:"type:text;not null" json:"material_info" validate:"required"`
+    BasePrice    decimal.Decimal `gorm:"type:decimal(10,2);not null" json:"base_price" validate:"required"`
+    Category     string          `gorm:"not null" json:"category" validate:"required"`
+    ImageURLs    []string        `gorm:"type:text[]" json:"image_urls"`
+    CreatedBy    string          `gorm:"type:uuid" json:"created_by" validate:"required,uuid"`
+    UpdatedBy    string          `gorm:"type:uuid" json:"updated_by" validate:"required,uuid"`
+    CreatedAt    time.Time       `gorm:"autoCreateTime" json:"created_at"`
+    UpdatedAt    time.Time       `gorm:"autoUpdateTime" json:"updated_at"`
+    Stocks       []Stock         `gorm:"constraint:OnDelete:SET NULL" json:"stocks"`
 }
 
 // レコード作成前にUUID v7を自動生成する
