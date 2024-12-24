f18485f0725ccaedaf0f9350005b091104b17a54
Sota Anegawa <sota_anegawa@pj100.biz>
2024-12-24 11:40:27 +0900

    initdb 削除

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -4,17 +4,15 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
-	"github.com/riii111/go-rust-comparison/internal/adapter/database"
 	"github.com/riii111/go-rust-comparison/internal/adapter/middleware"
 	"github.com/riii111/go-rust-comparison/internal/adapter/routes"
 )
 
 func main() {
-	database.InitDB()
 
 	r := gin.Default()
-	routes.SetupRoutes(r)
 	middleware.CORSConfig()
+	routes.SetupRoutes(r)
 
 	if err := r.Run(":8000"); err != nil {
 		log.Fatalf("サーバの起動に失敗しました: %v", err)
