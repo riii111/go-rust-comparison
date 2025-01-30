554873ca6a82ea3211ed9f2b433b2d27680c3214
Sota Anegawa <sota_anegawa@pj100.biz>
2025-01-31 08:10:42 +0900

    👍 エラーメッセージの一般化

--- backend/internal/infrastructure/repository/operator_repository.go
+++ backend/internal/infrastructure/repository/operator_repository.go
@@ -10,7 +10,7 @@ import (
 
 // データベースエラーに関する定数
 var (
-	ErrDuplicateEmail     = errors.New("既に登録されているメールアドレスです")
+	ErrDuplicateEmail     = errors.New("アカウント登録に失敗しました")
 	ErrForeignKeyViolated = errors.New("該当する店舗が存在しません")
 )
 
