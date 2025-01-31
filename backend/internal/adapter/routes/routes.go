00d90ff339eebb7a43bed51d820d42b83e48806d
藤田英之 <fujitahideyuki@helpdesknonotobukkukonpyuta-6.local>
2025-01-31 12:15:11 +0900

    routerにしまう

--- backend/internal/adapter/routes/routes.go
+++ backend/internal/adapter/routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/riii111/go-rust-comparison/internal/adapter/database"
 	"github.com/riii111/go-rust-comparison/internal/application/usecase"
 	"github.com/riii111/go-rust-comparison/internal/infrastructure/repository"
 	"github.com/riii111/go-rust-comparison/internal/presentation/handlers"
@@ -20,11 +21,21 @@ func setupOperatorRoutes(api *gin.RouterGroup) {
 	operators.POST("", operatorHandler.CreateOperator)
 }
 
+func setupStockRoutes(api *gin.RouterGroup) {
+	stocks := api.Group("/stocks")
+	stockRepo := repository.NewStockRepository(database.DB)
+	stockUseCase := usecase.NewStockUseCase(stockRepo)
+	stockHan := handlers.NewStockHandler(stockUseCase)
+
+	stocks.POST("", stockHan.CreateStock)
+}
+
 // メインのルーティング設定関数
 func SetupRoutes(r *gin.Engine) {
 	api := r.Group("/api")
 	{
 		setupHealthRoutes(api)
 		setupOperatorRoutes(api)
+		setupStockRoutes(api)
 	}
 }
