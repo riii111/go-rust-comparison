2b0219fc5fc3dd807f99186b991881afc29c32d4
Suguru Sasuo <165745452+SuguruSasuo@users.noreply.github.com>
2025-02-07 08:45:58 +0900

    コメント対応

--- backend/internal/adapter/database/database.go
+++ backend/internal/adapter/database/database.go
@@ -29,16 +29,16 @@ func getDBConfig() *DBConfig {
 		Host:     os.Getenv("POSTGRES_HOST"),
 		Port:     os.Getenv("POSTGRES_PORT"),
 		DBName:   os.Getenv("POSTGRES_NAME"),
+		SSlMode:  os.Getenv("POSTGRES_SSLMODE"),
 	}
 }
 
 // DSN文字列を生成
 func (c *DBConfig) buildDSN() string {
 	// ローカルと本番とでsslmodeの設定を変える
-	sslmode := os.Getenv("POSTGRES_SSLMODE")
 	return fmt.Sprintf(
 		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
-		c.User, c.Password, c.Host, c.Port, c.DBName, sslmode,
+		c.User, c.Password, c.Host, c.Port, c.DBName, c.SSlMode,
 	)
 }
 
