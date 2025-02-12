c574a113e330838f402e201f4ade4f808a8e2191
藤田英之 <fujitahideyuki@helpdesknonotobukkukonpyuta-6.local>
2025-02-12 14:16:24 +0900

    バリデーション追加

--- backend/internal/presentation/requests/stock_request.go
+++ backend/internal/presentation/requests/stock_request.go
@@ -5,11 +5,11 @@ import (
 )
 
 type CreateStockInput struct {
-	ProductID   string          `json:"product_id" binding:"required"`
-	StoreID     string          `json:"store_id" binding:"required"`
+	ProductID   string          `json:"product_id" binding:"required,uuid"`
+	StoreID     string          `json:"store_id" binding:"required,uuid"`
 	Size        string          `json:"size" binding:"required"`
 	Color       string          `json:"color" binding:"required"`
-	Quantity    uint            `json:"quantity" binding:"required"`
-	Price       decimal.Decimal `json:"price" binding:"required"`
+	Quantity    uint            `json:"quantity" binding:"required,max=9999"`
+	Price       decimal.Decimal `json:"price" binding:"required,price_range"`
 	IsAvailable bool            `json:"is_available" binding:"required"`
 }
